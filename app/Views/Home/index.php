427b0fc93843edd658892f2c51a58bb103cf6048
philander18 <philander.totalis@gmail.com>
2025-03-03 14:24:18 +0700

    Membuat tampilan home

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,68 +1,36 @@
 <?= $this->extend('Templates/index'); ?>
 <?= $this->section('page-content'); ?>
 <?= $this->include('Templates/nav'); ?>
-<!-- <section class="container-fluid hero" id="hero">
-    <div class="judul-hero fw-bold">
-        <h1>KKR Paskah 2025<br>Pelprap GPdI Wilayah 1 Bandung</h1>
-    </div>
-    <div class="waktu-hero">
-        <div class="card">
-            <h4>Dilaksanakan pada</h4>
-            <div class="isi-waktu-hero">
-                <div class="isi">
-                    <h4>Hari</h4>
-                    <h4>: Sabtu</h4>
-                </div>
-                <div class="isi">
-                    <h4>Tanggal</h4>
-                    <h4>: 26 April 2025</h4>
-                </div>
-                <div class="isi">
-                    <h4>Waktu</h4>
-                    <h4>: 16:00 - 18:30 WIB</h4>
-                </div>
-                <div class="isi">
-                    <h4>Tempat</h4>
-                    <h4>: GPdI Bukit Hermon</h4>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="tema-hero">
-        <h2>Rise Up, Rising Generation</h2>
-    </div>
-</section> -->
 <div class="page-phil">
-    <section class="satu-page">
-        <div class="layout-contents layout-hero">
-            <div class="judul-hero layout-subcontents">
-                <h5>Area Judul</h5>
+    <section class="satu-page area-hero">
+        <div class="layout-hero">
+            <div class="judul-hero">
+                <H3>KEBAKTIAN KEBANGUNAN ROHANI (KKR)<br>
+                    PASKAH PEMUDA DAN REMAJA<br>
+                    TAHUN 2025</H3>
             </div>
-            <div class="waktu-hero layout-subcontents">
-                <h5>Area Waktu dan Tempat</h5>
+            <div class="waktu-hero">
+                <h3>Waktu dan Tempat</h3>
+                <div class="isi-waktu">
+                    <h4><span>Hari, Tanggal</span>: Sabtu, 26 April 2025</h4>
+                    <h4><span>Waktu</span>: 16:00 - 19:00 WIB</h4>
+                    <h4><span>Tempat</span>: GPdI Bukit Hermon</h4>
+                    <h4>Jalan Mahar Martanegara No. 103, Cigugur Tengah, Kota Cimahi</h4>
+                </div>
             </div>
-            <div class="pembicara-hero layout-subcontents">
-                <h5>Area Pembicara</h5>
+            <div class="pembicara-hero">
+                <h3>Pembicara</h3>
+                <div><img src="<?= base_url(); ?>public/images/pembicara.png" alt="Pembicara"></div>
+                <h4><Span class="text-decoration-underline">Pdt. Vera Siwi</Span><br>Ketua KP Pelpap GPdI</h4>
             </div>
-            <div class="tema-hero layout-subcontents">
-                <h5>Area Tema</h5>
+            <div class="tema-hero">
+                <h3>Tema</h3>
+                <h4 class="text-decoration-underline">RISE UP, RISING GENERATION</h4>
+                <h4>Isaiah 60:1 (UKJV)</h4>
+                <h4>Arise, shine; for thy light is come, and the glory of the LORD is risen upon thee.</h4>
             </div>
-        </div>
-    </section>
-    <section class="satu-page">
-        <div class="layout-contents">
-            <h5>Area RAB</h5>
-        </div>
-    </section>
-    <section class="satu-page">
-        <div class="layout-contents">
-            <h5>Area Acara</h5>
         </div>
     </section>
 </div>
-<div class="satu-page footer-page">
-    <div class="layout-contents">
-        <h5>Footer</h5>
-    </div>
-</div>
+<?= $this->include('Templates/footer'); ?>
 <?= $this->endSection(); ?>
