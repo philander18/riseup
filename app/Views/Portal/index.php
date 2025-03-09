3ce1cfee584ac41d4e940e9b53f825e0ccb92b4a
philander18 <philander.totalis@gmail.com>
2025-03-09 22:57:40 +0700

    Mengganti tombol back dengan link

--- app/Views/Portal/index.php
+++ app/Views/Portal/index.php
@@ -12,7 +12,7 @@
             </div>
             <input class="form-control" type="password" name="kode" placeholder="Masukkan Kode" aria-label="Kode">
             <button type="Submit" class="btn-submit btn btn-dark fw-bold">Submit</button>
-            <button onclick="window.history.go(-1);" class="btn-back btn btn-dark fw-bold">Back</button>
+            <a class="btn-back btn btn-dark fw-bold" href="<?= base_url(); ?>" role="button">Back</a>
         </div>
     </div>
 </form>
