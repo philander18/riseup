f0e328674435b021530a1cf92564c1963aacf082
philander18 <philander.totalis@gmail.com>
2025-03-17 10:44:38 +0700

    menambahkan video panitia

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -21,7 +21,7 @@
             <div class="pembicara-hero">
                 <h3>Pembicara</h3>
                 <div><img src="<?= base_url(); ?>public/images/pembicara.png" alt="Pembicara"></div>
-                <h4><Span class="text-decoration-underline">Pdt. Vera Patita Siwi S. E.</Span><br>Ketua KP Pelpap GPdI</h4>
+                <h4><Span class="text-decoration-underline">Pdt. Vera Patita Siwi S. E.</Span><br>Ketua Komisi Pusat Pelpap GPdI</h4>
             </div>
             <div class="tema-hero">
                 <h3>Tema</h3>
@@ -34,7 +34,7 @@
     <section class="section-1">
         <div class="konten-phil sambutan-panitia">
             <p style="text-indent: 0;">Shalom, Salam sejahtera bagi kita semua,</p>
-            <p>Puji dan syukur kita panjatkan kepada Tuhan Yesus Kristus yang telah memberikan kesempatan berharga ini, sehingga kami panitia KKR Paskah 2025 Pelprap GPdI Wilayah 1 bisa dibentuk dan dilantik pada tanggal 25 Januari 2025 untuk mempersiapkan acara KKR Paskah 2025 pada tanggal 26 April 2025. Momen ini merupakan sebuah kabar sukacita bagi kita semua, khususnya bagi pemuda dan remaja di seluruh GPdI Wilayah 1 Bandung. Melalui acara ini, kita akan diberi kesempatan untuk beribadah Bersama, mengalami hadirat Tuhan dan terlebih bisa mempererat persekutuan pemuda dan remaja.</p>
+            <p>Puji dan syukur kita panjatkan kepada Tuhan Yesus Kristus yang telah memberikan kesempatan berharga ini, sehingga kami panitia KKR Paskah 2025 Pelprap GPdI Wilayah 1 Bandung bisa dibentuk dan dilantik pada tanggal 25 Januari 2025 untuk mempersiapkan acara KKR Paskah 2025 pada tanggal 26 April 2025. Momen ini merupakan sebuah kabar sukacita bagi kita semua, khususnya bagi pemuda dan remaja di seluruh GPdI Wilayah 1 Bandung. Melalui acara ini, kita akan diberi kesempatan untuk beribadah Bersama, mengalami hadirat Tuhan dan terlebih bisa mempererat persekutuan pemuda dan remaja.</p>
             <p>Panitia mengambil tema <span class="fw-bold">"Rise Up Rising Generation"</span>, kami sangat berharap melalui acara ini ada kuasa kebangkitan yang nyata dan dialami secara spiritual khususnya bagi pemuda dan remaja di seluruh GPdI Wilayah 1 Bandung. Sehingga dengan kebangkitan yang dialami setiap pribadi, lahirlah generasi kebangkitan yang akan menyebarluaskan Injil Kerajaan Sorga baik secara pribadi maupun organisasi atau komunitas, khususnya di Kota/Kabupaten Bandung dan sekitarnya.</p>
             <p>Tentu dalam pelaksanaan acara ini, kami juga menyadari bahwa berbagai dukungan sangat dibutuhkan, terutama dalam hal dana. Oleh karena itu, kami dengan rendah hati mengajak seluruh pihak untuk berkontribusi dalam mendukung acara ini sehingga acara berjalan dengan baik dan hanya nama Tuhan saja yang dimuliakan. Rencana total anggaran yang diperlukan untuk kebutuhan acara ini adalah Rp 36.000.000,00.</p>
             <p class="mb-0">Panitia membuka kontribusi dukungan dana sebagai berikut :</p>
@@ -46,18 +46,22 @@
                     <br>Donasi atau persembahan dari orang terdekat/kawan yang terkumpul dan tercatat secara kolektif oleh anggota pelprap GPdI Wilayah 1 Bandung pada sebuah kartu yang disediakan panitia.
                 </li>
                 <li style="text-align: justify;">Sponsorship
-                    <br>Bentuk dukungan dana yang diberikan oleh suatu pihak (perusahaan, organisasi, atau individu) untuk acara ini dengan adanya timbal balik mendapatkan manfaat tertentu yaitu promosi melalui media sosial/informasi panitia. Bagi yang tertarik dalam kerjasamanya ini bisa menghubungi Bpk. Rony Gunawan (081310961630) untuk bisa dijelaskan lebih detail terkait bentuk kerjasama dan benefitnya.
+                    <br>Bentuk dukungan dana yang diberikan oleh suatu pihak (perusahaan, organisasi, atau individu) untuk acara ini dengan adanya timbal balik mendapatkan manfaat tertentu yaitu promosi melalui media sosial/informasi panitia. Bagi yang tertarik dalam kerjasamanya ini bisa menghubungi Kak Rony Gunawan (081310961630) untuk bisa dijelaskan lebih detail terkait bentuk kerjasama dan benefitnya.
                 </li>
                 <li style="text-align: justify;">Penjualan Merchandise
                     <br>Penjualan merchandise yang seluruh keuntungan dari penjualan akan masuk ke kebutuhan acara ini. Bagi yang tertarik bisa langsung mengunjungi menu <a href="<?= base_url(); ?>shop" class="text-dark fw-bold" target="_blank">TOKO</a> pada website ini.
                 </li>
             </ol>
             <p>Setiap bentuk dukungan yang diberikan akan menjadi bagian dari pelayanan ini dan kami percaya bahwa Tuhan akan membalas setiap kebaikan dengan berkat yang melimpah.</p>
-            <p>Akhir kata, kami mengajak setiap pemuda dan remaja di seluruh GPdI Wilayah 1 Bandung untuk berpartisipasi aktif dalam acara ini. Bisa melakukan pendaftaran melalui website ini pada menu registrasi atau kolektif dari leader pelprap gereja masing-masing. Mari bersama-sama kita merayakan kebangkitan Kristus dengan sukacita, serta mengalami lawatan Tuhan yang mengubahkan hidup kita. Kiranya kasih dan damai sejahtera Tuhan menyertai kita semua.</p>
+            <p>Akhir kata, kami mengajak setiap pemuda dan remaja di seluruh GPdI Wilayah 1 Bandung untuk berpartisipasi aktif dalam acara ini. Bisa melakukan pendaftaran melalui website ini pada menu <a href="<?= base_url(); ?>registrasi" class="text-dark fw-bold" target="_blank">REGISTRASI</a> atau kolektif dari leader pelprap gereja masing-masing. Mari bersama-sama kita merayakan kebangkitan Kristus dengan sukacita, serta mengalami lawatan Tuhan yang mengubahkan hidup kita. Kiranya kasih dan damai sejahtera Tuhan menyertai kita semua.</p>
             <p style="text-indent: 0;">Tuhan Yesus memberkati!
                 <br>Panitia KKR Paskah 2025 Pelprap GPdI Wilayah 1 Bandung
             </p>
-            <img src="<?= base_url(); ?>public/images/ketua.png" alt="Ketua">
+            <div class="konten-video">
+                <video autoplay muted loop controls>
+                    <source src="<?= base_url(); ?>public/video/panitia.mp4" type="video/mp4">
+                </video>
+            </div>
         </div>
     </section>
 </div>
